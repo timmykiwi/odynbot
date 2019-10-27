8abdf7381bd0051cfc600cd773ed9eb32d8eb143
timmykiwi <57061878+timmykiwi@users.noreply.github.com>
2019-10-27 11:47:25 +0100

    Create say.js

---
+++ commands/moderation/say.js
@@ -0,0 +1,26 @@
+const { RichEmbed } = require("discord.js");
+
+module.exports = {
+    name: "mów",
+    aliases: ["bc", "broadcast"],
+    description: "Mówienie przez bota.",
+    usage: "<input>",
+    run: (client, message, args) => {
+        message.delete();
+
+        if (!message.member.hasPermission("MANAGE_MESSAGES"))
+            return message.reply("Nie masz permisji do tej komendy.").then(m => m.delete(5000));
+
+        const roleColor = message.guild.me.highestRole.hexColor;
+
+        if (args[0].toLowerCase() === "embed") {
+            const embed = new RichEmbed()
+                .setDescription(args.slice(1).join(" "))
+                .setColor(roleColor === "#000000" ? "#ffffff" : roleColor);
+
+            message.channel.send(embed);
+        } else {
+            message.channel.send(args.join(" "));
+        }
+    }
+}
